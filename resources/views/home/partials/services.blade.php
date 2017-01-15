05cf4e2814407f943d24edfdb49f2b266f3fbd32
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-15 23:52:59 +0000

    Remove root

--- resources/views/home/partials/services.blade.php
+++ resources/views/home/partials/services.blade.php
@@ -67,6 +67,22 @@
                             </div>
                         </div>
                     </div><!--.services row end-->
+
+                    <div class="row margin-btm-20">
+                        <div class="col-md-2">
+                            <div class="services-icon">
+                                <i class="fa fa-bullhorn"></i>
+                            </div>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="services-info">
+                                <h3>Palestras</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget viverra augue. Nunc dignissim rutrum tempor.
+                                </p>
+                            </div>
+                        </div>
+                    </div><!--.services row end-->
                 </div>
 
                 <div class="col-md-6 wow animated fadeInRight" data-wow-delay="0.6s">
@@ -97,21 +113,6 @@
                                 <p>
                                     <p>Compreendemos nosso dever social e escolhemos dispor de alguns horários para atendimento em clínica social. Ou seja, clientes que buscam atendimento psicológico e não tem a possibilidade de arcar com o custo integral de uma psicoterapia, tem a oportunidade de iniciar os atendimentos de acordo com um custo acertado na primeira entrevista de avaliação. Cada situação é avaliada individualmente, de acordo com a disponibilidade de horários do psicólogo e da possibilidade do cliente que busca atendimento.</p>
                                     <p>Caso tenha interesse em realizar uma entrevista para atendimento social, entre em contato conosco para agendar.</p>
-                                </p>
-                            </div>
-                        </div>
-                    </div><!--.services row end-->
-                    <div class="row margin-btm-20">
-                        <div class="col-md-2">
-                            <div class="services-icon">
-                                <i class="fa fa-bullhorn"></i>
-                            </div>
-                        </div>
-                        <div class="col-md-10">
-                            <div class="services-info">
-                                <h3>Palestras</h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget viverra augue. Nunc dignissim rutrum tempor.
                                 </p>
                             </div>
                         </div>
