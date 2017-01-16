dd10a862053a52714aba784952d13cbd38c0e266
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-16 02:16:19 +0000

    Logo 2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -50,7 +50,7 @@
                     <!-- Home Heading -->
                     <div class="tp-caption sft Ken-burns-heading"
                          data-x="center"
-                         data-y="310"
+                         data-y="330"
                          data-speed="1200"
                          data-start="1100"
                          data-easing="Power3.easeInOut"
